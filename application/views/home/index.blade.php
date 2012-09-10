d91fc2ebe35caf130316973b6afa893f3a0e4852
Dave Shoreman <codeM0nK3Y@me.com>
2012-09-10 17:49:53 +0100

    Made items draggable and created drop/sort area

--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -7,16 +7,21 @@
 @section('content')
 <div class="row">
 	<div class="span5" id="items">
-		@foreach ($items as $item)
-			<div class="well">
-				<img src="{{ $url($item->alias?:$item->name) }}" title="{{ $item->name }}" />
-			</div>
-		@endforeach
+		<ul>
+			@foreach ($items as $item)
+				<li class="well">
+					<img src="{{ $url($item->alias?:$item->name) }}" title="{{ $item->name }}" />
+					<span>{{ $item->name }}</span>
+				</li>
+			@endforeach
+		</ul>
 	</div>
 	<div class="span7">
 		<div class="row">
 			<div id="inventory">
-				<div class="well"></div>
+				<div class="well">
+					<ul id="stage"></ul>
+				</div>
 			</div>
 		</div>
 		<div class="row">
@@ -26,5 +31,25 @@
 		</div>
 	</div>
 </div>
+@endsection
 
+@section('scripts')
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.min.js"></script>
+<script type="text/javascript">
+	$(document).ready(function()
+	{
+		$('#stage').sortable({
+			placeholder: 'info well',
+			revert: true,
+		});
+		$('#items .well').draggable({
+			connectToSortable: '#stage',
+			helper: 'clone',
+			revert: 'invalid',
+		});
+		$('#stage, #items').disableSelection();
+
+		$('#items .well img').tooltip();
+	})
+</script>
 @endsection
